6e7a212bd7500bffd79d26f4abc23087a84069f0
Hector Sebastian Choque Dueñas <hector.choque@utec.edu.pe>
2026-09-21 01:59:00 -0500

    Se añade README y cierto cambios al .gitignore para considerar al .env

--- src/main/java/com/hackathon/hackathon1/Hackathon1Application.java
+++ src/main/java/com/hackathon/hackathon1/Hackathon1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Hackathon1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Hackathon1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon1Application.class, args);
+    }
 
 }
